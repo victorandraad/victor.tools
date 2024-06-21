c980c623a6e4581f8c7b3db0173bbf106fe96dad
vouto <68758638+andradavic@users.noreply.github.com>
2024-06-21 20:01:25 -0300

    Actualizar explanation.vue

--- roleta/src/components/explanation.vue
+++ roleta/src/components/explanation.vue
@@ -5,7 +5,7 @@
             <p>Essa ferramente oferece uma maneira fácil e eficiente de sortear times para jogos. Basta seguir alguns passos simples e você terá os tiomes definidos rapidamente</p>
         </div>
 
-        <div class="flex gap-12 pt-20 spacebetween ">
+        <div class="flex gap-12 pt-20 spacebetween flex-wrap">
             <div class="flex flex-col gap-2 text-start">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10">
                     <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
